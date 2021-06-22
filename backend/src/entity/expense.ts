e52ce72cab5cab4a4a34b4381379b87449602114
John Boamah <joboa2015@gmail.com>
2021-06-22 16:56:31 +0000

    added db config

--- backend/src/entity/expense.ts
+++ backend/src/entity/expense.ts
@@ -1,6 +1,20 @@
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+
+@Entity()
 export class Expense {
+
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column("double")
   amount: number;
+
+  @Column({
+    length: 150
+  })
   description: string;
+
+  @Column()
   date: Date;
 }
+
