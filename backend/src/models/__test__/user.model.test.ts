d0142d31bae81a739f658f5a6ed72e8b5343850a
John Boamah <joboa2015@gmail.com>
2021-06-25 16:32:16 +0000

    added user model test

---
+++ backend/src/models/__test__/user.model.test.ts
@@ -0,0 +1,40 @@
+import { User } from '../user'
+import { getRepository } from 'typeorm'
+
+describe('User model', () => {
+    describe('schema', () => {
+        test('firstName', () => {
+            const user = new User()
+            user.id = 1
+            user.firstName = 'firstName'
+            user.lastName = 'lastName'
+            user.email = 'email@email.com'
+            user.password = 'password'
+            expect(user).toEqual({
+                id: 1,
+                firstName: 'firstName',
+                lastName: 'lastName',
+                email: 'email@email.com',
+                password: 'password',
+            })
+        })
+
+        // test('lastName', () => {
+        //   const user = new User
+        //   user.lastName= "lastName"
+        //   expect(user).toEqual({lastName: "lastName"})
+        // })
+
+        // test('email', () => {
+        //   const user = new User
+        //   user.email= "email@email.com"
+        //   expect(user).toEqual({email: "email@email.com"})
+        // })
+
+        // test('password', () => {
+        //   const user = new User
+        //   user.password = "password"
+        //   expect(user).toEqual({password: "password"})
+        // })
+    })
+})
