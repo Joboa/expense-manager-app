ec4fa58fe788755f9ad6c6e424d8ada1590c236e
John Boamah <joboa2015@gmail.com>
2021-06-25 23:18:55 +0000

    updated crud controllers for user

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,19 +1,53 @@
-import { Request, Response} from 'express'
+import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
-import {User} from '../models'
+import { User } from '../models'
 
 class UserController {
-  static userCreate = async (req: Request, res: Response) => {
-    const newUser = {
-      firstName : req.body.firstName,
-      lastName : req.body.lastName,
-      email : req.body.email,
-      password : req.body.password,
+    static createUser = async (req: Request, res: Response) => {
+        const newUser = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+        }
+        const user = getRepository(User).create(newUser)
+        const createUserResult = await getRepository(User).save(user)
+        return res.status(200).json(createUserResult)
     }
-    const user = getRepository(User).create(newUser)
-    const result = await getRepository(User).save(user)
-    return res.json(result)
-  }
+
+    static getUsers = async (req: Request, res: Response) => {
+        const getUsersResult = await getRepository(User).find()
+        getUsersResult
+            ? res.status(200).json(getUsersResult)
+            : res.status(400).send({ message: 'No users found!' })
+    }
+
+    static getUser = async (req: Request, res: Response) => {
+        const id = req.params.id
+        const getUserResult = await getRepository(User).findOne(id)
+        if (!getUserResult)
+            return res.status(400).send({ message: 'No user found!' })
+        return res.json(getUserResult)
+    }
+
+    static updateUser = async (req: Request, res: Response) => {
+        const id = req.params.id
+        const updateUserInfo = await getRepository(User).findOne(id)
+        if (updateUserInfo) {
+            getRepository(User).merge(updateUserInfo, req.body)
+            const updatedUserInfo = await getRepository(User).save(
+                updateUserInfo
+            )
+            return res.json(updatedUserInfo)
+        }
+        return res.json({ message: 'User not found!' })
+    }
+
+    static deleteUser = async (req: Request, res: Response) => {
+        const id = req.params.id
+        const deleteAUser = await getRepository(User).delete(id)
+        return res.json(deleteAUser)
+    }
 }
 
 export default UserController
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,8 +1,12 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import UserController from '../controllers/user.controller'
 
 const router = Router()
 
-router.post('/user', UserController.userCreate)
+router.post('/user', UserController.createUser)
+router.get('/users', UserController.getUsers)
+router.get('/user/:id', UserController.getUser)
+router.patch('/user/:id', UserController.updateUser)
+router.delete('/user/:id', UserController.deleteUser)
 
 export default router
