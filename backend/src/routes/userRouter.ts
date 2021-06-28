978f7212ca7389d817d524e946a9ad399014ed15
John Boamah <joboa2015@gmail.com>
2021-06-28 17:31:46 +0000

    Update user router with PUT

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -6,7 +6,7 @@ const router = Router()
 router.post('/users', UserController.createUser)
 router.get('/users', UserController.getUsers)
 router.get('/users/:id', UserController.getUser)
-router.patch('/users/:id', UserController.updateUser)
+router.put('/users/:id', UserController.updateUser)
 router.delete('/users/:id', UserController.deleteUser)
 
 export default router
