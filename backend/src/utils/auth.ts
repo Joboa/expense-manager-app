59c8f4708ebcf4ad57a055b72b727fb4371ff2bd
John Boamah <joboa2015@gmail.com>
2021-07-01 10:54:58 +0000

    Add user authentication

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,3 +1,17 @@
-import { jwtconfig } from '../config/jwt.config'
 import { User } from '../models'
 import jwt from 'jsonwebtoken'
+
+export const newToken = (user: any) => {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRATION,
+  })
+}
+
+export const verifyToken = (token: any) => {
+  new Promise((resolve: any, reject: any) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err: any, payload: any) => {
+      if (err) return reject(err)
+      resolve(payload)
+    })
+  })
+}
