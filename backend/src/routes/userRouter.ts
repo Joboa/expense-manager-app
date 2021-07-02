eda2624fc59cc68d65dc42e8a57795fc3acfe0ac
John Boamah <joboa2015@gmail.com>
2021-07-02 10:16:24 +0000

    fixup! Update user router

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express'
-import controllers from '../controllers/user.controller'
+import UserController from '../controllers/user.controller'
 
 const router = Router()
 
 // /api/v1/users/
 router.route('/')
-  .get(controllers.getMany)
-  .post(controllers.createOne)
+  .get(UserController.getUsers)
+  .post(UserController.createUser)
 
 // /api/v1/users/:id
 router
   .route('/:id')
-  .get(controllers.getOne)
-  .put(controllers.updateOne)
-  .delete(controllers.removeOne)
+  .get(UserController.getUser)
+  .put(UserController.updateUser)
+  .delete(UserController.deleteUser)
 
 export default router
